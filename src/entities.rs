78aa5765f8be4e5e647aa31247ee8b6bdd88b2e2
vwiart <vincent.wiart@gmail.com>
2019-03-18 08:44:57 +0100

    refactoring

--- src/entities.rs
+++ src/entities.rs
@@ -1,9 +1,5 @@
-use amethyst::core::Transform;
 use amethyst::prelude::{Builder, GameData, SimpleState, StateData, World};
 use amethyst::renderer::{Camera, Projection};
-
-pub const DISPLAY_WIDTH: f32 = 512.;
-pub const DISPLAY_HEIGHT: f32 = 320.;
 
 pub struct InitialState;
 
